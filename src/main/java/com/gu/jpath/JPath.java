7dd4d3762506dd47494320268ef1a622a4bb03e5
Phil Wills <otherphil@gmail.com>
2009-12-30 16:09:50 +0000

    Flipped rount JPath interface so that is based around the path itself

--- src/main/java/com/gu/jpath/JPath.java
+++ src/main/java/com/gu/jpath/JPath.java
@@ -1,35 +1,29 @@
 package com.gu.jpath;
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
 public class JPath {
-	private JsonElement root;
+	private List<QueryToken> queryTokens;
 
-	public JPath(InputStream jsonStream) {
-		JsonParser parser = new JsonParser();
-		root = parser.parse(new InputStreamReader(jsonStream));
-	}
-
-	public JsonElement forPath(String path) {
+	public JPath(String path) {
 		JPathLexer lexer = new JPathLexer(new ANTLRStringStream(path));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         JPathParser parser = new JPathParser(tokens);
-		List<QueryToken> queryTokens = null;
+		
 		try {
 			queryTokens = parser.query();
 		} catch (RecognitionException e) {
 			throw new JPathException(e);
 		}
+	}
 
-		JsonElement currentElement = root;
+	public JsonElement fromElement(JsonElement element) {
+		JsonElement currentElement = element;
 		for (QueryToken token: queryTokens) {
 			currentElement = token.navigate(currentElement);
 		}
--- src/test/java/com/gu/jpath/JPathTest.java
+++ src/test/java/com/gu/jpath/JPathTest.java
@@ -1,27 +1,36 @@
 package com.gu.jpath;
 
 import org.junit.Test;
+import org.junit.Before;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class JPathTest {
+	private JsonElement exampleRoot;
+	
 	@Test public void shouldFindJSONContentBasedOnPath() {
-		JPath jPath = new JPath(getCheeseStream());
+		JPath jPath = new JPath("glossary.title");
 
-		assertThat(jPath.forPath("glossary.title").getAsString(), equalTo("example glossary"));
+		assertThat(jPath.fromElement(exampleRoot).getAsString(), equalTo("example glossary"));
 	}
 
 	@Test public void shouldFindJSONContentInArrayElement() {
-		JPath jPath = new JPath(getCheeseStream());
+		JPath jPath = new JPath("glossary.GlossDiv.GlossList.GlossEntry.GlossDef.GlossSeeAlso[1]");
 
-		assertThat(jPath.forPath("glossary.GlossDiv.GlossList.GlossEntry.GlossDef.GlossSeeAlso[1]").getAsString(), equalTo("XML"));
+		assertThat(jPath.fromElement(exampleRoot).getAsString(), equalTo("XML"));
 	}
 
-	private InputStream getCheeseStream() {
+	@Before
+	public void buildExampleRoot() {
 		InputStream jsonStream = JPathTest.class.getResourceAsStream("example.json");
-		return jsonStream;
+		JsonParser parser = new JsonParser();
+		exampleRoot = parser.parse(new InputStreamReader(jsonStream));
 	}
 }
