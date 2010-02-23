058eeae4b3d4de9152ef336b98bcfdee78340c9d
Phil Wills <otherphil@gmail.com>
2010-02-23 00:22:08 +0000

    Tidying up unnecessary imports

--- core/src/main/scala/JPath.scala
+++ core/src/main/scala/JPath.scala
@@ -1,15 +1,6 @@
 package com.gu.jpath
 
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 class JPath(path: String) {
 	val queryTokens = JsonArgoGrammar.tokens(path)
@@ -17,9 +8,7 @@ class JPath(path: String) {
 	def from(element: JsonElement) : java.util.List[JsonElement] = descend(queryTokens, element)
 	
 	private def descend(tokens: List[QueryToken], element: JsonElement) : List[JsonElement] = {
-		if (tokens.isEmpty) 
-			List(element) 
-		else {
+		if (tokens.isEmpty) List(element) else {
 			for { 
 				navigatedElement <- tokens.head.navigate(element)
 				descendedElement <- descend(tokens.drop(1), navigatedElement)
@@ -28,7 +17,7 @@ class JPath(path: String) {
 	}
 	
 	implicit def scalaToJavaList(scalaList: List[JsonElement]): java.util.List[JsonElement] = {
-		var javaList = new ArrayList[JsonElement]()
+		var javaList = new java.util.ArrayList[JsonElement]()
 		scalaList foreach(javaList.add(_))
 		javaList
 	}
