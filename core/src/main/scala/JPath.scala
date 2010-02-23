3837f96a8c9906a5ccfeecb8b1917eba6ddab2df
Phil Wills <otherphil@gmail.com>
2010-02-23 22:36:47 +0000

    More joyfully unnecessary finessing

--- core/src/main/scala/JPath.scala
+++ core/src/main/scala/JPath.scala
@@ -5,15 +5,16 @@ import com.google.gson.JsonElement;
 class JPath(path: String) {
 	val queryTokens = JsonArgoGrammar.tokens(path)
 
-	def from(element: JsonElement) : java.util.List[JsonElement] = descend(queryTokens, element)
-	
-	private def descend(tokens: List[QueryToken], element: JsonElement) : List[JsonElement] = {
-		if (tokens.isEmpty) List(element) else {
-			for { 
-				navigatedElement <- tokens.head.navigate(element)
-				descendedElement <- descend(tokens.drop(1), navigatedElement)
-			} yield descendedElement
+	def from(startElement: JsonElement) : java.util.List[JsonElement] = {
+		def descend(tokens: List[QueryToken], element: JsonElement) : List[JsonElement] = {
+			if (tokens isEmpty) List(element) else {
+				for { 
+					navigatedElement <- tokens.head.navigate(element)
+					descendedElement <- descend(tokens.tail, navigatedElement)
+				} yield descendedElement
+			}
 		}
+		descend(queryTokens, startElement)
 	}
 	
 	implicit def scalaToJavaList(scalaList: List[JsonElement]): java.util.List[JsonElement] = {
