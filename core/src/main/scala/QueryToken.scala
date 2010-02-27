44e083a1a449bb25c8f07cd4a6a0026e28b520e0
Phil Wills <otherphil@gmail.com>
2010-02-27 22:53:01 +0000

    Removing a teensy bit of duplication and made iterator conversion implicit

--- core/src/main/scala/QueryToken.scala
+++ core/src/main/scala/QueryToken.scala
@@ -2,38 +2,33 @@ package com.gu.jpath
 
 import com.google.gson.JsonElement
 
-trait QueryToken {
-	def navigate(element: JsonElement) : List[JsonElement]
+abstract class QueryToken(identifier: String) {
+	def navigate(element: JsonElement) : List[JsonElement] = from(element.getAsJsonObject().get(identifier))
+	def from(element: JsonElement) : List[JsonElement]
 }
 
 object QueryToken {
-	private class DirectAccessQueryToken(identifier: String) extends QueryToken {
-		override def navigate(element: JsonElement) = {
-			List(element.getAsJsonObject().get(identifier))
-		}	
+	private class DirectAccessQueryToken(identifier: String) extends QueryToken(identifier) {
+		override def from(element: JsonElement) = List(element)
 		override def toString = identifier
 	}
 
-	private class ArrayAccessQueryToken(identifier: String, index: Int) extends QueryToken {
-		override def navigate(element: JsonElement) = {
-			List(element.getAsJsonObject().get(identifier).getAsJsonArray().get(index));
-		}
+	private class ArrayAccessQueryToken(identifier: String, index: Int) extends QueryToken(identifier) {
+		override def from(element: JsonElement) = List(element.getAsJsonArray().get(index))
 		override def toString = identifier + "[" + index + "]"
 	}
 
-	private class WholeArrayQueryToken(identifier: String)  extends QueryToken {
-		override def navigate(element: JsonElement) = {
-			List.fromIterator(new ScalaJavaIterator(element.getAsJsonObject.get(identifier).getAsJsonArray.iterator))
-		}
+	private class WholeArrayQueryToken(identifier: String) extends QueryToken(identifier) {
+		override def from(element: JsonElement) = List.fromIterator(element.getAsJsonArray.iterator)
 		override def toString = identifier + "[*]"
 	}
 
 	def apply(identifier: String) : QueryToken = new DirectAccessQueryToken(identifier)
 	def apply(identifier: String, index: Int) : QueryToken = new ArrayAccessQueryToken(identifier, index)
 	def all(identifier: String) : QueryToken = new WholeArrayQueryToken(identifier)
-}
-
-class ScalaJavaIterator[T](private val iterator: java.util.Iterator[T]) extends Iterator[T] {
-  def hasNext: Boolean = iterator hasNext
-  def next: T = iterator next
+	
+	implicit def javaToScalaIterator[T](iterator: java.util.Iterator[T]) : Iterator[T] = new Iterator[T] {
+	  def hasNext: Boolean = iterator hasNext
+	  def next: T = iterator next
+	}
 }
